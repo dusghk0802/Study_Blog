ce8694a754917d3600523b6a058e11448eb88e9e
dusghk0802 <jjydusghk03@gmail.com>
2026-08-07 15:51:30 +0900

    Add files via upload

---
+++ training/Java/2026.08.07/chap10/interfaceex/CalculatorTest.java
@@ -0,0 +1,14 @@
+package chap10.interfaceex;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+
+        int num1 = 10;
+        int num2 = 5;
+
+        Calc calc = new CompleteCalc();
+        System.out.println(calc.add(num1, num2));
+        System.out.println(calc.subtract(num1, num2));
+
+    }
+}
