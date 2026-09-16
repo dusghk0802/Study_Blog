cffaf981dd6a5eb02630212dc403b7ab32a3f3f4
dusghk0802 <jjydusghk03@gmail.com>
2026-09-16 16:28:22 +0900

    Add files via upload

---
+++ training/React/2026.09.16/react01-basic/App.jsx
@@ -0,0 +1,18 @@
+import FrontComp from './components/FrontComp';
+import BackComp from './components/BackComp';
+
+function App() {
+  return (<>
+    <h2>React-Event</h2>
+    <ol>
+      <FrontComp onMyEvent1={()=>{
+        alert('프론트엔드 클릭됨(부모전달)');
+      }}></FrontComp>
+      <BackComp onMyEvent2={(msg)=>{
+        alert(msg);
+      }}/>
+    </ol>
+  </>)
+}
+
+export default App
