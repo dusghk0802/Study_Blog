63902cd90e7da9956cca8e46bf2eaed5b0162dc8
dusghk0802 <jjydusghk03@gmail.com>
2026-09-19 15:20:30 +0900

    Update TopNavi.jsx

--- training/React/2026.09.19/react03-lifecycle/components/TopNavi.jsx
+++ training/React/2026.09.19/react03-lifecycle/components/TopNavi.jsx
@@ -6,6 +6,8 @@ const TopNavi = () => {
       <NavLink to="/">수명주기</NavLink>&nbsp;&nbsp;
       <NavLink to="/local">내부통신</NavLink>&nbsp;&nbsp;
       <NavLink to="/external">외부통신</NavLink>&nbsp;&nbsp;
+      <NavLink to="/localbook">내부통신1</NavLink>&nbsp;&nbsp;
+      <NavLink to="/weather">외부통신1</NavLink>&nbsp;&nbsp;
     </nav>
   )
 }
