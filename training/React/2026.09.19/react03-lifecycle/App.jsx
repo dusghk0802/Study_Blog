30a641c7be2c18d248e3ca7e32fd829aa4003af0
dusghk0802 <jjydusghk03@gmail.com>
2026-09-19 15:21:02 +0900

    Update App.jsx

--- training/React/2026.09.19/react03-lifecycle/App.jsx
+++ training/React/2026.09.19/react03-lifecycle/App.jsx
@@ -3,6 +3,8 @@ import TopNavi from './components/TopNavi.jsx'
 import LifeCycle from './components/Lifecycle.jsx'
 import LocalJsonFetcher from './components/LocalJsonFetcher.jsx'
 import ExternalJsonFetcher from './components/ExternalApiFetcher.jsx'
+import LocalBookFetcher from './components/LocalBookFetcher.jsx'
+import WeatherFetcher from './components/WeatherFetcher.jsx' 
 
 function App() {
 
@@ -12,6 +14,8 @@ function App() {
       <Route path="/" element={<LifeCycle />} />
        <Route path="/local" element={<LocalJsonFetcher />} />
        <Route path="/external" element={<ExternalJsonFetcher />} />
+        <Route path="/localbook" element={<LocalBookFetcher />} />
+        <Route path="/weather" element={<WeatherFetcher />} />
     </Routes>
     </>)
 }
