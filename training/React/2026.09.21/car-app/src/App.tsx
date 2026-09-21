f3bafdbe299529cc2ee96b7de840fc9b864428f2
dusghk0802 <jjydusghk03@gmail.com>
2026-09-21 15:39:30 +0900

    Add files via upload

---
+++ training/React/2026.09.21/car-app/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import CarList from './compooents/CarList.tsx'
+
+function App() {
+  return (<>
+      <h2>Car Application</h2>
+      <CarList />
+    </>)
+}
+
+export default App
